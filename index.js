f842f0ac9cedc593d8e84e3504cae0d6d86f4f95
chloe <dotdotclover@gmail.com>
2019-04-19 22:28:28 -0400

    ap/token/fights per hour

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function rateCompare() {
   
+  //fight constants
+  var exTokens = 56;
+  var expTokens = 66;
+  var nm90Tokens = 83;
+  var nm95Tokens = 111;
+  var nm100Tokens = 148;
+
+  var exAP = 30;
+  var expAP = 30;
+  var nm90AP = 30;
+  var nm95AP = 40;
+  var nm100AP = 50;
+  
   //honors rates
   var exHonors = parseFloat(document.getElementById("exHonorsInput").value);
   var expHonors = parseFloat(document.getElementById("expHonorsInput").value);
@@ -7,17 +20,19 @@ function rateCompare() {
   var nm95Honors = parseFloat(document.getElementById("nm95HonorsInput").value);
   var nm100Honors = parseFloat(document.getElementById("nm100HonorsInput").value);
   
-  //clear times from form
+  //gets the times from the form
   var exClearTimeMin = parseFloat(document.getElementById("exTimeInputMin").value);
   var exClearTimeSec = parseFloat(document.getElementById("exTimeInputSec").value);
   var exClearTimeMin = exClearTimeMin*60;
   var exClearTime = exClearTimeSec+exClearTimeMin;
+  
 
   
   var expClearTimeMin = parseFloat(document.getElementById("expTimeInputMin").value);
   var expClearTimeSec = parseFloat(document.getElementById("expTimeInputSec").value);
   var expClearTimeMin = expClearTimeMin*60;
   var expClearTime = expClearTimeSec+expClearTimeMin;
+  var expClearPerHour = 60/expClearTime;
   
   var nm90ClearTimeMin = parseFloat(document.getElementById("nm90TimeInputMin").value);
   var nm90ClearTimeSec = parseFloat(document.getElementById("nm90TimeInputSec").value);
@@ -33,6 +48,43 @@ function rateCompare() {
   var nm100ClearTimeSec = parseFloat(document.getElementById("nm100TimeInputSec").value);
   var nm100ClearTimeMin = nm100ClearTimeMin*60;
   var nm100ClearTime = nm100ClearTimeSec+nm100ClearTimeMin;
+  
+  //using clears per hour for token gain and ap use / writing to page
+  //ex+
+  var expClearPerHour = 3600/expClearTime;
+  var expTPH = expTokens*expClearPerHour;
+  var expAPUPH = expClearPerHour*expAP;
+  document.getElementById("expAPUPH").innerHTML = Math.round(expAPUPH);
+  document.getElementById("expTPH").innerHTML = Math.round(expTPH);
+  document.getElementById("expfph").innerHTML = Math.round(expClearPerHour);
+  //ex
+  var exClearPerHour = 3600/exClearTime;
+  var exTPH = exTokens*exClearPerHour;
+  var exAPUPH = exClearPerHour*exAP;
+  document.getElementById("exAPUPH").innerHTML = Math.round(exAPUPH);
+  document.getElementById("exTPH").innerHTML = Math.round(exTPH);
+  document.getElementById("exfph").innerHTML = Math.round(exClearPerHour);
+  //nm90
+  var nm90ClearPerHour = 3600/nm90ClearTime;
+  var nm90TPH = nm90Tokens*nm90ClearPerHour;
+  var nm90APUPH = nm90ClearPerHour*nm90AP;
+  document.getElementById("nm90APUPH").innerHTML = Math.round(nm90APUPH);
+  document.getElementById("nm90TPH").innerHTML = Math.round(nm90TPH);
+  document.getElementById("nm90fph").innerHTML = Math.round(nm90ClearPerHour);
+  //nm95
+  var nm95ClearPerHour = 3600/nm95ClearTime;
+  var nm95TPH = nm95Tokens*nm95ClearPerHour;
+  var nm95APUPH = nm95ClearPerHour*nm95AP;
+  document.getElementById("nm95APUPH").innerHTML = Math.round(nm95APUPH);
+  document.getElementById("nm95TPH").innerHTML = Math.round(nm95TPH);
+  document.getElementById("nm95fph").innerHTML = Math.round(nm95ClearPerHour);
+  //nm100
+  var nm100ClearPerHour = 3600/nm100ClearTime;
+  var nm100TPH = nm100Tokens*nm100ClearPerHour;
+  var nm100APUPH = nm100ClearPerHour*nm100AP;
+  document.getElementById("nm100APUPH").innerHTML = Math.round(nm100APUPH);
+  document.getElementById("nm100TPH").innerHTML = Math.round(nm100TPH);
+  document.getElementById("nm100fph").innerHTML = Math.round(nm100ClearPerHour);
 
   //divides honors by time to get gain per second
   var exHPS = exHonors/exClearTime;
@@ -40,7 +92,6 @@ function rateCompare() {
   var nm90HPS = nm90Honors/nm90ClearTime;
   var nm95HPS = nm95Honors/nm95ClearTime;
   var nm100HPS = nm100Honors/nm100ClearTime;
-  
   
   //converts honors per sec to honors per min and fills out the chart
   var exHPM = exHPS*3600;
