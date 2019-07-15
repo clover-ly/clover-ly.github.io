30f2915c252c1caccbf0b6c685022822e5b71b6a
cloverly <chlosparkle@gmail.com>
2019-07-15 09:42:25 -0400

    MORE STICKERS
    ESSENTIAL UPDATE

--- gw2/index.js
+++ gw2/index.js
@@ -1,4 +1,4 @@
-var imgArr = ["https://gbf.wiki/images/b/be/Stamp133.png","https://gbf.wiki/images/f/fd/Stamp242.png","https://gbf.wiki/images/7/71/Stamp54.png","https://gbf.wiki/images/b/bd/Stamp169.png","https://gbf.wiki/images/f/fc/Stamp62.png","https://gbf.wiki/images/c/cd/Stamp94.png","https://gbf.wiki/images/d/d3/Stamp177.png","https://gbf.wiki/images/c/cd/Stamp94.png"];
+var imgArr = ["https://gbf.wiki/images/b/be/Stamp133.png","https://gbf.wiki/images/f/fd/Stamp242.png","https://gbf.wiki/images/7/71/Stamp54.png","https://gbf.wiki/images/b/bd/Stamp169.png","https://gbf.wiki/images/f/fc/Stamp62.png","https://gbf.wiki/images/c/cd/Stamp94.png","https://gbf.wiki/images/d/d3/Stamp177.png","https://gbf.wiki/images/c/cd/Stamp94.png","https://gbf.wiki/images/7/7c/Stamp79.png","https://gbf.wiki/images/c/c4/Stamp141.png","https://gbf.wiki/images/4/4d/Stamp41.png","https://gbf.wiki/images/e/e8/Stamp261.png"];
 
 function displayImg(){
     var num = Math.floor(Math.random() * (imgArr.length));
