f33c8e52f6d28dcbd8694db5cbd93e6c83ae83fc
cloverly <chlosparkle@gmail.com>
2019-11-10 17:51:55 -0500

    calculate by boxes!

--- gw2/index.js
+++ gw2/index.js
@@ -1,11 +1,22 @@
-var imgArr = ["https://gbf.wiki/images/b/be/Stamp133.png","https://gbf.wiki/images/f/fd/Stamp242.png","https://gbf.wiki/images/7/71/Stamp54.png","https://gbf.wiki/images/b/bd/Stamp169.png","https://gbf.wiki/images/f/fc/Stamp62.png","https://gbf.wiki/images/c/cd/Stamp94.png","https://gbf.wiki/images/d/d3/Stamp177.png","https://gbf.wiki/images/c/cd/Stamp94.png","https://gbf.wiki/images/7/7c/Stamp79.png","https://gbf.wiki/images/c/c4/Stamp141.png","https://gbf.wiki/images/4/4d/Stamp41.png","https://gbf.wiki/images/e/e8/Stamp261.png"];
+var imgArr = [
+  "https://gbf.wiki/images/b/be/Stamp133.png",
+  "https://gbf.wiki/images/f/fd/Stamp242.png",
+  "https://gbf.wiki/images/7/71/Stamp54.png",
+  "https://gbf.wiki/images/b/bd/Stamp169.png",
+  "https://gbf.wiki/images/f/fc/Stamp62.png",
+  "https://gbf.wiki/images/c/cd/Stamp94.png",
+  "https://gbf.wiki/images/d/d3/Stamp177.png",
+  "https://gbf.wiki/images/c/cd/Stamp94.png",
+  "https://gbf.wiki/images/7/7c/Stamp79.png",
+  "https://gbf.wiki/images/c/c4/Stamp141.png",
+  "https://gbf.wiki/images/4/4d/Stamp41.png",
+  "https://gbf.wiki/images/e/e8/Stamp261.png"
+];
 
-function displayImg(){
-    var num = Math.floor(Math.random() * (imgArr.length));
-    document.sticker.src=imgArr[num];
+function displayImg() {
+  var num = Math.floor(Math.random() * imgArr.length);
+  document.sticker.src = imgArr[num];
 }
-
-
 
 // shows and hides raid data when boxes are checked. raid parameter is which div to hide. works for anything
 function hideDataCard(raid) {
@@ -26,8 +37,6 @@ function calculate() {
   var st = document.getElementById("goalSelect");
   var goalType = st.options[st.selectedIndex].value;
   document.getElementById("reportGoalTypeDisplay").innerHTML = goalType;
-  
-  
 
   //fight constants
   var exTokens = 56;
@@ -41,7 +50,7 @@ function calculate() {
   var nm90AP = 30;
   var nm95AP = 40;
   var nm100AP = 50;
-  
+
   var exMeats = 3;
   var expMeats = 4;
   var nm90Meats = 5;
@@ -54,7 +63,9 @@ function calculate() {
   var expHonors = parseFloat(document.getElementById("expHonorsInput").value);
   var nm90Honors = parseFloat(document.getElementById("nm90HonorsInput").value);
   var nm95Honors = parseFloat(document.getElementById("nm95HonorsInput").value);
-  var nm100Honors = parseFloat(document.getElementById("nm100HonorsInput").value);
+  var nm100Honors = parseFloat(
+    document.getElementById("nm100HonorsInput").value
+  );
 
   //gets the times from the form and converts it to seconds
   var exClearTimeMin = parseFloat(
@@ -87,15 +98,22 @@ function calculate() {
   var nm90ClearTimeMin = nm90ClearTimeMin * 60;
   var nm90ClearTime = nm90ClearTimeSec + nm90ClearTimeMin;
 
-  var nm95ClearTimeMin = parseFloat(document.getElementById("nm95TimeInputMin").value);
-  var nm95ClearTimeSec = parseFloat(document.getElementById("nm95TimeInputSec").value);
+  var nm95ClearTimeMin = parseFloat(
+    document.getElementById("nm95TimeInputMin").value
+  );
+  var nm95ClearTimeSec = parseFloat(
+    document.getElementById("nm95TimeInputSec").value
+  );
   var nm95ClearTimeMin;
   var nm95ClearTimeMin = nm95ClearTimeMin * 60;
   var nm95ClearTime = nm95ClearTimeSec + nm95ClearTimeMin;
 
-  var nm100ClearTimeMin = parseFloat(document.getElementById("nm100TimeInputMin").value);
+  var nm100ClearTimeMin = parseFloat(
+    document.getElementById("nm100TimeInputMin").value
+  );
   var nm100ClearTimeSec = parseFloat(
-  document.getElementById("nm100TimeInputSec").value);
+    document.getElementById("nm100TimeInputSec").value
+  );
   var nm100ClearTimeMin;
   var nm100ClearTimeMin = nm100ClearTimeMin * 60;
   var nm100ClearTime = nm100ClearTimeSec + nm100ClearTimeMin;
@@ -105,6 +123,13 @@ function calculate() {
   //goal material gain per raid.
   //change this to get the honors from the form
   switch (goalType) {
+    case " Boxes":
+      var gainPerEx = 56;
+      var gainPerExp = 66;
+      var gainPerNm90 = 83;
+      var gainPerNm95 = 111;
+      var gainPerNm100 = 148;
+      break;
     case " Honors":
       var gainPerEx = 51000;
       var gainPerExp = 72000;
@@ -129,166 +154,202 @@ function calculate() {
     default:
   }
   //from here on gainPer[raid] refers to either tokens honors or meats depending on what was selected.
-  
-  //how much of each to reach goal based on goal amount and gain rate by goal type
-  var totalGoalEx = Math.ceil(goalNumber / gainPerEx);
-  var totalGoalExp = Math.ceil(goalNumber / gainPerExp);
-  var totalGoalNm90 = Math.ceil(goalNumber / gainPerNm90);
-  var totalGoalNm95 = Math.ceil(goalNumber / gainPerNm95);
-  var totalGoalNm100 = Math.ceil(goalNumber / gainPerNm100);
-  
-  
+
+  //if the mode is boxes do the box math, else do the stuff under the other commetn
+
+  if (goalType === " Boxes") {
+    //so, counting for boxes is the same as counting for tokens basically
+    //this is the step that converts number of boxes into number of tokens
+    //we do this by changing the goalNumber given in boxes to tokens based on the conversion numbers
+
+    //PARAM: a goalNumber 1-80. needs to be converted into token number
+    
+    if (goalNumber==1){
+      goalNumber = 1600;
+    } else if (goalNumber<5 && goalNumber>1) {
+      goalNumber = 1600+((goalNumber-1)*2400);
+    } else if (goalNumber>4 && goalNumber<45) {
+      goalNumber = 8800+((goalNumber-4)*2000);
+    } else if (goalNumber>44) {
+      goalNumber = 80800+((goalNumber-4)*6000);
+    }
+
+    var totalGoalEx = Math.ceil(goalNumber / gainPerEx);
+    var totalGoalExp = Math.ceil(goalNumber / gainPerExp);
+    var totalGoalNm90 = Math.ceil(goalNumber / gainPerNm90);
+    var totalGoalNm95 = Math.ceil(goalNumber / gainPerNm95);
+    var totalGoalNm100 = Math.ceil(goalNumber / gainPerNm100);
+    
+  } else {
+    //how much of each to reach goal based on goal amount and gain rate by goal type
+    var totalGoalEx = Math.ceil(goalNumber / gainPerEx);
+    var totalGoalExp = Math.ceil(goalNumber / gainPerExp);
+    var totalGoalNm90 = Math.ceil(goalNumber / gainPerNm90);
+    var totalGoalNm95 = Math.ceil(goalNumber / gainPerNm95);
+    var totalGoalNm100 = Math.ceil(goalNumber / gainPerNm100);
+  }
+
   //for each raid type how much of other resources it takes based on the number being done to reach the goal as found above
-  
-  var totalGoalExAP = totalGoalEx*exAP;
-  var totalGoalExpAP = totalGoalExp*expAP;
-  var totalGoalNm90AP = totalGoalNm90*nm90AP;
-  var totalGoalNm95AP = totalGoalNm95*nm95AP;
-  var totalGoalNm100AP = totalGoalNm100*nm100AP;
-  
-  var totalGoalExHonors = totalGoalEx*exHonors;
-  var totalGoalExpHonors = totalGoalExp*expHonors;
-  var totalGoalNm90Honors = totalGoalNm90*nm90Honors;
-  var totalGoalNm95Honors = totalGoalNm95*nm95Honors;
-  var totalGoalNm100Honors = totalGoalNm100*nm100Honors;
-  
-  var totalGoalExClearTime = totalGoalEx*exClearTime;
-  var totalGoalExpClearTime = totalGoalExp*expClearTime;
-  var totalGoalNm90ClearTime = totalGoalNm90*nm90ClearTime;
-  var totalGoalNm95ClearTime = totalGoalNm95*nm95ClearTime;
-  var totalGoalNm100ClearTime = totalGoalNm100*nm100ClearTime;
-  
-  var totalGoalExMeat = totalGoalEx*exMeats;
-  var totalGoalExpMeat = totalGoalExp*expMeats;
-  var totalGoalNm90Meat = totalGoalNm90*nm90Meats;
-  var totalGoalNm95Meat = totalGoalNm95*nm95Meats;
-  var totalGoalNm100Meat = totalGoalNm100*nm100Meats;
-  
-  var totalGoalExTokens = totalGoalEx*exTokens;
-  var totalGoalExpTokens = totalGoalExp*expTokens;
-  var totalGoalNm90Tokens = totalGoalNm90*nm90Tokens;
-  var totalGoalNm95Tokens = totalGoalNm95*nm95Tokens;
-  var totalGoalNm100Tokens = totalGoalNm100*nm100Tokens;
-  
+
+  var totalGoalExAP = totalGoalEx * exAP;
+  var totalGoalExpAP = totalGoalExp * expAP;
+  var totalGoalNm90AP = totalGoalNm90 * nm90AP;
+  var totalGoalNm95AP = totalGoalNm95 * nm95AP;
+  var totalGoalNm100AP = totalGoalNm100 * nm100AP;
+
+  var totalGoalExHonors = totalGoalEx * exHonors;
+  var totalGoalExpHonors = totalGoalExp * expHonors;
+  var totalGoalNm90Honors = totalGoalNm90 * nm90Honors;
+  var totalGoalNm95Honors = totalGoalNm95 * nm95Honors;
+  var totalGoalNm100Honors = totalGoalNm100 * nm100Honors;
+
+  var totalGoalExClearTime = totalGoalEx * exClearTime;
+  var totalGoalExpClearTime = totalGoalExp * expClearTime;
+  var totalGoalNm90ClearTime = totalGoalNm90 * nm90ClearTime;
+  var totalGoalNm95ClearTime = totalGoalNm95 * nm95ClearTime;
+  var totalGoalNm100ClearTime = totalGoalNm100 * nm100ClearTime;
+
+  var totalGoalExMeat = totalGoalEx * exMeats;
+  var totalGoalExpMeat = totalGoalExp * expMeats;
+  var totalGoalNm90Meat = totalGoalNm90 * nm90Meats;
+  var totalGoalNm95Meat = totalGoalNm95 * nm95Meats;
+  var totalGoalNm100Meat = totalGoalNm100 * nm100Meats;
+
+  var totalGoalExTokens = totalGoalEx * exTokens;
+  var totalGoalExpTokens = totalGoalExp * expTokens;
+  var totalGoalNm90Tokens = totalGoalNm90 * nm90Tokens;
+  var totalGoalNm95Tokens = totalGoalNm95 * nm95Tokens;
+  var totalGoalNm100Tokens = totalGoalNm100 * nm100Tokens;
+
   //calculate pots by divinding ap bvy 75
-  
-  var totalGoalExPots = Math.ceil(totalGoalExAP/75);
-  var totalGoalExpPots = Math.ceil(totalGoalExpAP/75);
-  var totalGoalNm90Pots = Math.ceil(totalGoalNm90AP/75);
-  var totalGoalNm95Pots = Math.ceil(totalGoalNm95AP/75);
-  var totalGoalNm100Pots = Math.ceil(totalGoalNm100AP/75);
-   
+
+  var totalGoalExPots = Math.ceil(totalGoalExAP / 75);
+  var totalGoalExpPots = Math.ceil(totalGoalExpAP / 75);
+  var totalGoalNm90Pots = Math.ceil(totalGoalNm90AP / 75);
+  var totalGoalNm95Pots = Math.ceil(totalGoalNm95AP / 75);
+  var totalGoalNm100Pots = Math.ceil(totalGoalNm100AP / 75);
+
   //total time in seconds to mins and secs for readability
 
   var exTimeString = new Date(null);
-  exTimeString.setSeconds(totalGoalExClearTime); 
+  exTimeString.setSeconds(totalGoalExClearTime);
   var exFinalTime = exTimeString.toISOString().substr(11, 8);
 
   var expTimeString = new Date(null);
-  expTimeString.setSeconds(totalGoalExpClearTime); 
+  expTimeString.setSeconds(totalGoalExpClearTime);
   var expFinalTime = expTimeString.toISOString().substr(11, 8);
-  
+
   var nm90TimeString = new Date(null);
-  nm90TimeString.setSeconds(totalGoalNm90ClearTime); 
+  nm90TimeString.setSeconds(totalGoalNm90ClearTime);
   var nm90FinalTime = nm90TimeString.toISOString().substr(11, 8);
-  
+
   var nm95TimeString = new Date(null);
-  nm95TimeString.setSeconds(totalGoalNm95ClearTime); 
+  nm95TimeString.setSeconds(totalGoalNm95ClearTime);
   var nm95FinalTime = nm95TimeString.toISOString().substr(11, 8);
-  
+
   var nm100TimeString = new Date(null);
-  nm100TimeString.setSeconds(totalGoalNm100ClearTime); 
+  nm100TimeString.setSeconds(totalGoalNm100ClearTime);
   var nm100FinalTime = nm100TimeString.toISOString().substr(11, 8);
-  
-  
 
   //making the report
   //calculate for each raid what their speeds would take to reach the goal by each material.
   //display and calc them all, just hide the goal
-  
+
   //printouts
-  
-   if (document.getElementById("exCalcCheck").checked) {document.getElementById("reportEx").style.display = "inline-block";
-  document.getElementById("exFightCountDisplay").innerHTML = totalGoalEx;
-  document.getElementById("exTimeDisplay").innerHTML = exFinalTime;
-  document.getElementById("exHonorsDisplay").innerHTML = totalGoalExHonors;
-  document.getElementById("exTokenDisplay").innerHTML = totalGoalExTokens;
-  document.getElementById("exMeatsDisplay").innerHTML = totalGoalExMeat;
-  document.getElementById("exAPDisplay").innerHTML = totalGoalExAP;
-document.getElementById("exPotsDisplay").innerHTML = totalGoalExPots;}else{
-  document.getElementById("reportEx").style.display = "none";  }
-  
 
-    if (document.getElementById("expCalcCheck").checked) {document.getElementById("reportExp").style.display = "inline-block";
-  document.getElementById("expFightCountDisplay").innerHTML = totalGoalExp;
-  document.getElementById("expTimeDisplay").innerHTML = expFinalTime;
-  document.getElementById("expHonorsDisplay").innerHTML = totalGoalExpHonors;
-  document.getElementById("expTokenDisplay").innerHTML = totalGoalExpTokens;
-  document.getElementById("expMeatsDisplay").innerHTML = totalGoalExpMeat;
-  document.getElementById("expAPDisplay").innerHTML = totalGoalExpAP;
-document.getElementById("expPotsDisplay").innerHTML = totalGoalExpPots;}else{
-  document.getElementById("reportExp").style.display = "none";  }
-  
-  
-  if (document.getElementById("nm90CalcCheck").checked) {document.getElementById("reportnm90").style.display = "inline-block";
-  document.getElementById("nm90FightCountDisplay").innerHTML = totalGoalNm90;
-  document.getElementById("nm90TimeDisplay").innerHTML = nm90FinalTime;
-  document.getElementById("nm90HonorsDisplay").innerHTML = totalGoalNm90Honors;
-  document.getElementById("nm90TokenDisplay").innerHTML = totalGoalNm90Tokens;
-  document.getElementById("nm90MeatsDisplay").innerHTML = totalGoalNm90Meat;
-  document.getElementById("nm90APDisplay").innerHTML = totalGoalNm90AP;
-document.getElementById("reportnm90").style.display = "inline-block";                                                       document.getElementById("nm90PotsDisplay").innerHTML = totalGoalNm90Pots;
-}else{
-  document.getElementById("reportnm90").style.display = "none";  }
+  if (document.getElementById("exCalcCheck").checked) {
+    document.getElementById("reportEx").style.display = "inline-block";
+    document.getElementById("exFightCountDisplay").innerHTML = totalGoalEx;
+    document.getElementById("exTimeDisplay").innerHTML = exFinalTime;
+    document.getElementById("exHonorsDisplay").innerHTML = totalGoalExHonors;
+    document.getElementById("exTokenDisplay").innerHTML = totalGoalExTokens;
+    document.getElementById("exMeatsDisplay").innerHTML = totalGoalExMeat;
+    document.getElementById("exAPDisplay").innerHTML = totalGoalExAP;
+    document.getElementById("exPotsDisplay").innerHTML = totalGoalExPots;
+  } else {
+    document.getElementById("reportEx").style.display = "none";
+  }
 
-  
-   if (document.getElementById("nm95CalcCheck").checked){
-     document.getElementById("reportnm95").style.display = "inline-block";
- document.getElementById("nm95FightCountDisplay").innerHTML = totalGoalNm95;
-  document.getElementById("nm95TimeDisplay").innerHTML = nm95FinalTime;
-  document.getElementById("nm95HonorsDisplay").innerHTML = totalGoalNm95Honors;
-  document.getElementById("nm95TokenDisplay").innerHTML = totalGoalNm95Tokens;
-  document.getElementById("nm95MeatsDisplay").innerHTML = totalGoalNm95Meat;
-  document.getElementById("nm95APDisplay").innerHTML = totalGoalNm95AP;document.getElementById("nm95PotsDisplay").innerHTML = totalGoalNm95Pots;}else{
-  document.getElementById("reportnm95").style.display = "none";  }
-  
- if (document.getElementById("nm100CalcCheck").checked){
-   document.getElementById("reportnm100").style.display = "inline-block";  
-  document.getElementById("nm100FightCountDisplay").innerHTML = totalGoalNm100;
-  document.getElementById("nm100TimeDisplay").innerHTML = nm100FinalTime;
-  document.getElementById("nm100HonorsDisplay").innerHTML = totalGoalNm100Honors;
-  document.getElementById("nm100TokenDisplay").innerHTML = totalGoalNm100Tokens;
-  document.getElementById("nm100MeatsDisplay").innerHTML = totalGoalNm100Meat;
-  document.getElementById("nm100APDisplay").innerHTML = totalGoalNm100AP;
-   document.getElementById("nm100PotsDisplay").innerHTML = totalGoalNm100Pots;}else{
-  document.getElementById("reportnm100").style.display = "none";  }
-  
-  
-  
-  
+  if (document.getElementById("expCalcCheck").checked) {
+    document.getElementById("reportExp").style.display = "inline-block";
+    document.getElementById("expFightCountDisplay").innerHTML = totalGoalExp;
+    document.getElementById("expTimeDisplay").innerHTML = expFinalTime;
+    document.getElementById("expHonorsDisplay").innerHTML = totalGoalExpHonors;
+    document.getElementById("expTokenDisplay").innerHTML = totalGoalExpTokens;
+    document.getElementById("expMeatsDisplay").innerHTML = totalGoalExpMeat;
+    document.getElementById("expAPDisplay").innerHTML = totalGoalExpAP;
+    document.getElementById("expPotsDisplay").innerHTML = totalGoalExpPots;
+  } else {
+    document.getElementById("reportExp").style.display = "none";
+  }
 
+  if (document.getElementById("nm90CalcCheck").checked) {
+    document.getElementById("reportnm90").style.display = "inline-block";
+    document.getElementById("nm90FightCountDisplay").innerHTML = totalGoalNm90;
+    document.getElementById("nm90TimeDisplay").innerHTML = nm90FinalTime;
+    document.getElementById(
+      "nm90HonorsDisplay"
+    ).innerHTML = totalGoalNm90Honors;
+    document.getElementById("nm90TokenDisplay").innerHTML = totalGoalNm90Tokens;
+    document.getElementById("nm90MeatsDisplay").innerHTML = totalGoalNm90Meat;
+    document.getElementById("nm90APDisplay").innerHTML = totalGoalNm90AP;
+    document.getElementById("reportnm90").style.display = "inline-block";
+    document.getElementById("nm90PotsDisplay").innerHTML = totalGoalNm90Pots;
+  } else {
+    document.getElementById("reportnm90").style.display = "none";
+  }
+
+  if (document.getElementById("nm95CalcCheck").checked) {
+    document.getElementById("reportnm95").style.display = "inline-block";
+    document.getElementById("nm95FightCountDisplay").innerHTML = totalGoalNm95;
+    document.getElementById("nm95TimeDisplay").innerHTML = nm95FinalTime;
+    document.getElementById(
+      "nm95HonorsDisplay"
+    ).innerHTML = totalGoalNm95Honors;
+    document.getElementById("nm95TokenDisplay").innerHTML = totalGoalNm95Tokens;
+    document.getElementById("nm95MeatsDisplay").innerHTML = totalGoalNm95Meat;
+    document.getElementById("nm95APDisplay").innerHTML = totalGoalNm95AP;
+    document.getElementById("nm95PotsDisplay").innerHTML = totalGoalNm95Pots;
+  } else {
+    document.getElementById("reportnm95").style.display = "none";
+  }
+
+  if (document.getElementById("nm100CalcCheck").checked) {
+    document.getElementById("reportnm100").style.display = "inline-block";
+    document.getElementById(
+      "nm100FightCountDisplay"
+    ).innerHTML = totalGoalNm100;
+    document.getElementById("nm100TimeDisplay").innerHTML = nm100FinalTime;
+    document.getElementById(
+      "nm100HonorsDisplay"
+    ).innerHTML = totalGoalNm100Honors;
+    document.getElementById(
+      "nm100TokenDisplay"
+    ).innerHTML = totalGoalNm100Tokens;
+    document.getElementById("nm100MeatsDisplay").innerHTML = totalGoalNm100Meat;
+    document.getElementById("nm100APDisplay").innerHTML = totalGoalNm100AP;
+    document.getElementById("nm100PotsDisplay").innerHTML = totalGoalNm100Pots;
+  } else {
+    document.getElementById("reportnm100").style.display = "none";
+  }
 }
 
 function material(x, y) {
- var labelSelect = document.getElementById(x);
-                                           var inputSelect = document.getElementById(y);
-                                     
-  
- labelSelect.style.color = "blue";
-  inputSelect.style.borderBottom= "2px solid blue";
+  var labelSelect = document.getElementById(x);
+  var inputSelect = document.getElementById(y);
 
-  }
-
-function material2(x, y) {
- //this one undoes it
-   var labelSelect = document.getElementById(x);
-                                           var inputSelect = document.getElementById(y);
-  
-  labelSelect.style.color = "#aaa";
-   inputSelect.style.borderBottom= "2px solid #ccc";
+  labelSelect.style.color = "blue";
+  inputSelect.style.borderBottom = "2px solid blue";
 }
 
+function material2(x, y) {
+  //this one undoes it
+  var labelSelect = document.getElementById(x);
+  var inputSelect = document.getElementById(y);
 
+  labelSelect.style.color = "#aaa";
+  inputSelect.style.borderBottom = "2px solid #ccc";
+}
 
 function popupclose() {
   var x = document.getElementById("popupwrap");
