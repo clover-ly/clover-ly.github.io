f1fa8b664e04a72ee8a20ea194a2c152db83792a
cloverly <chlosparkle@gmail.com>
2019-12-22 20:12:27 -0500

    nightmare 150

--- gw2/index.js
+++ gw2/index.js
@@ -44,28 +44,31 @@ function calculate() {
   var nm90Tokens = 83;
   var nm95Tokens = 111;
   var nm100Tokens = 148;
-
+  var nm150Tokens = 220;
+  
   var exAP = 30;
   var expAP = 30;
   var nm90AP = 30;
   var nm95AP = 40;
   var nm100AP = 50;
+  var nm150AP = 50;
 
   var exMeats = 3;
   var expMeats = 4;
   var nm90Meats = 5;
   var nm95Meats = 10;
   var nm100Meats = 20;
+  var nm150Meats = 20;
 
   //grab the inputed honors data
   var exHonors = parseFloat(document.getElementById("exHonorsInput").value);
-
   var expHonors = parseFloat(document.getElementById("expHonorsInput").value);
   var nm90Honors = parseFloat(document.getElementById("nm90HonorsInput").value);
   var nm95Honors = parseFloat(document.getElementById("nm95HonorsInput").value);
   var nm100Honors = parseFloat(
     document.getElementById("nm100HonorsInput").value
   );
+  var nm150Honors = parseFloat(document.getElementById("nm150HonorsInput").value);
 
   //gets the times from the form and converts it to seconds
   var exClearTimeMin = parseFloat(
@@ -117,6 +120,16 @@ function calculate() {
   var nm100ClearTimeMin;
   var nm100ClearTimeMin = nm100ClearTimeMin * 60;
   var nm100ClearTime = nm100ClearTimeSec + nm100ClearTimeMin;
+  
+  var nm150ClearTimeMin = parseFloat(
+    document.getElementById("nm150TimeInputMin").value
+  );
+  var nm150ClearTimeSec = parseFloat(
+    document.getElementById("nm150TimeInputSec").value
+  );
+  var nm150ClearTimeMin;
+  var nm150ClearTimeMin = nm150ClearTimeMin * 60;
+  var nm150ClearTime = nm150ClearTimeSec + nm150ClearTimeMin;
 
   //now u have the honors and clear time in seconds.
 
@@ -129,6 +142,7 @@ function calculate() {
       var gainPerNm90 = 83;
       var gainPerNm95 = 111;
       var gainPerNm100 = 148;
+      var gainPerNm150 = 220;
       break;
     case " Honors":
       var gainPerEx = 51000;
@@ -136,6 +150,7 @@ function calculate() {
       var gainPerNm90 = 260000;
       var gainPerNm95 = 910000;
       var gainPerNm100 = 2650000;
+      var gainPerNm150 = 3600000;
       break;
     case " Tokens":
       var gainPerEx = 56;
@@ -143,6 +158,7 @@ function calculate() {
       var gainPerNm90 = 83;
       var gainPerNm95 = 111;
       var gainPerNm100 = 148;
+      var gainPerNm150 = 220;
       break;
     case " Meats":
       var gainPerEx = 3;
@@ -150,6 +166,7 @@ function calculate() {
       var gainPerNm90 = 5;
       var gainPerNm95 = 10;
       var gainPerNm100 = 20;
+      var gainPerNm150 = 20;
       break;
     default:
   }
@@ -179,6 +196,7 @@ function calculate() {
     var totalGoalNm90 = Math.ceil(goalNumber / gainPerNm90);
     var totalGoalNm95 = Math.ceil(goalNumber / gainPerNm95);
     var totalGoalNm100 = Math.ceil(goalNumber / gainPerNm100);
+    var totalGoalNm150 = Math.ceil(goalNumber / gainPerNm150);
     
   } else {
     //how much of each to reach goal based on goal amount and gain rate by goal type
@@ -187,6 +205,7 @@ function calculate() {
     var totalGoalNm90 = Math.ceil(goalNumber / gainPerNm90);
     var totalGoalNm95 = Math.ceil(goalNumber / gainPerNm95);
     var totalGoalNm100 = Math.ceil(goalNumber / gainPerNm100);
+    var totalGoalNm150 = Math.ceil(goalNumber / gainPerNm150);
   }
 
   //for each raid type how much of other resources it takes based on the number being done to reach the goal as found above
@@ -196,30 +215,35 @@ function calculate() {
   var totalGoalNm90AP = totalGoalNm90 * nm90AP;
   var totalGoalNm95AP = totalGoalNm95 * nm95AP;
   var totalGoalNm100AP = totalGoalNm100 * nm100AP;
+  var totalGoalNm150AP = totalGoalNm150 * nm150AP;
 
   var totalGoalExHonors = totalGoalEx * exHonors;
   var totalGoalExpHonors = totalGoalExp * expHonors;
   var totalGoalNm90Honors = totalGoalNm90 * nm90Honors;
   var totalGoalNm95Honors = totalGoalNm95 * nm95Honors;
   var totalGoalNm100Honors = totalGoalNm100 * nm100Honors;
+  var totalGoalNm150Honors = totalGoalNm150 * nm150Honors;
 
   var totalGoalExClearTime = totalGoalEx * exClearTime;
   var totalGoalExpClearTime = totalGoalExp * expClearTime;
   var totalGoalNm90ClearTime = totalGoalNm90 * nm90ClearTime;
   var totalGoalNm95ClearTime = totalGoalNm95 * nm95ClearTime;
   var totalGoalNm100ClearTime = totalGoalNm100 * nm100ClearTime;
+  var totalGoalNm150ClearTime = totalGoalNm150 * nm150ClearTime;
 
   var totalGoalExMeat = totalGoalEx * exMeats;
   var totalGoalExpMeat = totalGoalExp * expMeats;
   var totalGoalNm90Meat = totalGoalNm90 * nm90Meats;
   var totalGoalNm95Meat = totalGoalNm95 * nm95Meats;
   var totalGoalNm100Meat = totalGoalNm100 * nm100Meats;
+  var totalGoalNm150Meat = totalGoalNm150 * nm150Meats;
 
   var totalGoalExTokens = totalGoalEx * exTokens;
   var totalGoalExpTokens = totalGoalExp * expTokens;
   var totalGoalNm90Tokens = totalGoalNm90 * nm90Tokens;
   var totalGoalNm95Tokens = totalGoalNm95 * nm95Tokens;
   var totalGoalNm100Tokens = totalGoalNm100 * nm100Tokens;
+  var totalGoalNm150Tokens = totalGoalNm150 * nm150Tokens;
 
   //calculate pots by divinding ap bvy 75
 
@@ -228,6 +252,7 @@ function calculate() {
   var totalGoalNm90Pots = Math.ceil(totalGoalNm90AP / 75);
   var totalGoalNm95Pots = Math.ceil(totalGoalNm95AP / 75);
   var totalGoalNm100Pots = Math.ceil(totalGoalNm100AP / 75);
+  var totalGoalNm150Pots = Math.ceil(totalGoalNm150AP / 75);
 
   //total time in seconds to mins and secs for readability
 
@@ -251,6 +276,10 @@ function calculate() {
   nm100TimeString.setSeconds(totalGoalNm100ClearTime);
   var nm100FinalTime = nm100TimeString.toISOString().substr(11, 8);
 
+  var nm150TimeString = new Date(null);
+  nm150TimeString.setSeconds(totalGoalNm150ClearTime);
+  var nm150FinalTime = nm150TimeString.toISOString().substr(11, 8);
+  
   //making the report
   //calculate for each raid what their speeds would take to reach the goal by each material.
   //display and calc them all, just hide the goal
@@ -331,6 +360,25 @@ function calculate() {
     document.getElementById("nm100PotsDisplay").innerHTML = totalGoalNm100Pots;
   } else {
     document.getElementById("reportnm100").style.display = "none";
+  }
+  
+  if (document.getElementById("nm150CalcCheck").checked) {
+    document.getElementById("reportnm150").style.display = "inline-block";
+    document.getElementById(
+      "nm150FightCountDisplay"
+    ).innerHTML = totalGoalNm150;
+    document.getElementById("nm150TimeDisplay").innerHTML = nm150FinalTime;
+    document.getElementById(
+      "nm150HonorsDisplay"
+    ).innerHTML = totalGoalNm150Honors;
+    document.getElementById(
+      "nm150TokenDisplay"
+    ).innerHTML = totalGoalNm150Tokens;
+    document.getElementById("nm150MeatsDisplay").innerHTML = totalGoalNm150Meat;
+    document.getElementById("nm150APDisplay").innerHTML = totalGoalNm150AP;
+    document.getElementById("nm150PotsDisplay").innerHTML = totalGoalNm150Pots;
+  } else {
+    document.getElementById("reportnm150").style.display = "none";
   }
 }
 
